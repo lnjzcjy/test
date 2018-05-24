8dfc45155e76ee2cfab92834d5024f6de5a48d73
lnjzcjy <lnjzcjy@163.com>
2018-05-24 15:43:21 +0800

    work done without a menu

--- DC/dc.py
+++ DC/dc.py
@@ -15,9 +15,19 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
          temp = self.spin_temp.value()
          pres = self.spin_pres.value()
          inpu = self.spin_inpu.value()
-         rightvalue = 0.0
-         error      = 0.0
-         output ="the right value is  "+ str(temp+pres+inpu)+"  and there is some error that is abuot  "+str(error)+" !"
+         Ktp        = (273.15+temp)*101.3/pres/293.15                 # ok
+         Kmkatt     = 0.972                                            # ok
+         nxR        = 5.546
+         Nx         = nxR *2.58e-4                                    # what is the unit? 2.58e-4C/(kg.nC) Nx in SI unit
+         Nd         = Nx*33.97*Kmkatt                                  #ok 33.97 j/kg
+         Swa        = 1.116                                            # ok                                                      
+         Pu         = 1.0                                            #ok
+         Pcel       = 1.0                                              #ok
+         inpu_ktp   = inpu * Ktp
+         dose       = inpu_ktp * Nd * Swa *Pu* Pcel*100.0
+         dose_setup = 86.6
+         error      = (dose / dose_setup)-1.0
+         output ="Ktp is  "+str(Ktp)+"\nDose is "+ str(dose)+"cgy\n error is "+str(error)+"\n"+ "\n\nhere is the calculate processing: \n Dw(Peff)=M*ND*Sw,air*Pu*Pcel\n we also know that the Dw(Peff) mean setup is chanble under water 5cm (5.2cm) field is 10cm*10cm 6MV photons pdd=86.6% so 100mu means "+str(dose_setup)+"cgy\n\nDw("+str(dose)+")=(M_KTP"+str(inpu_ktp)+"=M("+str(inpu)+")*Ktp("+str(Ktp)+"))*ND("+str(Nd)+")*Sw,a("+str(Swa)+")*Pu("+str(Pu)+")*Pcel("+str(Pcel)+")\n #Nd("+str(Nd)+")=Nx("+str(Nx)+") * W/e(33.97J/C)* Katt * Km(Kmkatt = "+str(Kmkatt)+")"+"\n#Nx("+str(Nx)+")=nxR("+str(nxR)+")*2.58e-4(C/kg.nC)"
          self.text_outp.setText(output)
 
 
